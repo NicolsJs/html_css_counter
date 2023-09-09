06a272c949f6524660cce748acf9ccdd48e31242
Nicole <nicolealternativo99@gmail.com>
2023-09-09 18:43:25 -0300

    first commit

---
+++ app/index.js
@@ -0,0 +1,16 @@
+/*var count = 0 
+
+$(document).getElementById(function(){
+  $("value").onclick(function(){
+
+    if ()
+
+  });
+}); */
+
+function btnCode(action) {
+  var counterelement = parseInt($("acounterView")).html());
+  var response = 0;
+
+}
+
